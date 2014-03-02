774bc36b77098acdef7b68ef73322b6f359fd148
matt <matt76k@gmail.com>
2014-03-02 16:10:51 +0900

    change slightly

--- examples/sort.hs
+++ examples/sort.hs
@@ -2,18 +2,18 @@
 import Language.MI.TH
 
 [mi|
-class DEq a where neq::a->a->Bool
-                  ndq::a->a->Bool
-                  gt:: a->a->Bool|]
+class DEq a where neq::a -> a -> Bool
+                  ndq::a -> a -> Bool
+                  gt:: a -> a -> Bool|]
 
-[mi|instance deq1::DEq Integer where
-       neq a b = (a `mod` 2 == 0) && (b `mod` 2 == 0)
-       ndq a b = (a `mod` 2 == 1) && (b `mod` 2 == 1)
+[mi|instance deq1::DEq Int where
+       neq a b = (a `mod` 2) == (b `mod` 2)
+       ndq a b = not $ (neq' a b) -- bug
        gt  a b = a < b|]
 
-[mi|instance deq2::DEq Integer where
-       neq a b = (a `mod` 3 == 0) && (b `mod` 3 == 0)
-       ndq a b = (a `mod` 3 == 1) && (b `mod` 3 == 1)
+[mi|instance deq2::DEq Int where
+       neq a b = (a `mod` 3) == (b `mod` 3)
+       ndq a b = (a `mod` 3) /= (b `mod` 3)
        gt a b = a > b|]
 
 [mi|
@@ -23,13 +23,16 @@ mi = deq1 where
 
 
 [mi|
-mi = deq1 where 
- qsort []     = []
- qsort (p:xs) = qsort [x | x <- xs, x `gt` p] ++ [p] ++ qsort [x | x <- xs, not $ x `gt` p] 
+mi = deq1 where
+  qsort :: [Int] -> [Int]
+  qsort []     = []
+  qsort (p:xs) = qsort [x | x <- xs, x `gt` p] ++ [p] ++ qsort [x | x <- xs, not $ x `gt` p] 
 |]
 
 main = do
     print $ qsort d
     print $ qsort' deq2 d 
+    print $ check $ qsort d
+    print $ check' deq2 $ qsort d
   where
     d = [2, 1, 4, 6, 3, 5]
